5a228e0261bf6d56ff036af39633ad4ebafc172b
“[rahma <[rahmahfaiedh5@gmail.com]>
2021-11-28 18:07:43 +0000

    FirstCommit

---
+++ src/Test/countA.java
@@ -0,0 +1,16 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class countA {
+
+	@Test
+	public void test() {
+		JunitTest test =new JunitTest();
+		int output = test.countA("alphabet");
+		assertEquals(2,output);
+	}
+
+}
