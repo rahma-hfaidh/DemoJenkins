8097d9f78c5fb455b3f3453abfef004effa43bae
“[rahma <[rahmahfaiedh5@gmail.com]>
2021-11-29 07:04:42 +0000

    commit

--- src/Test/countA.java
+++ src/Test/countA.java
@@ -10,7 +10,7 @@ public class countA {
 	public void test() {
 		JunitTest test =new JunitTest();
 		int output = test.countA("alphabet");
-		assertEquals(2,output);
+		assertEquals(3,output);
 	}
 
 }
